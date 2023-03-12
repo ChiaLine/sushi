c7aff1c307e114787953001f5c04327017ebe33a
egg <egg810207@gmail.com>
2023-03-12 12:48:43 +0800

    新增餐點圖

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>炙燒鮭魚三味 $180 & 炙燒日本松露干貝 3 貫 $180</h1>
+    <img src="../assets/imgs/炙燒三味鮭魚& 炙燒松露干貝 .JPG" alt="" width="450" height="">
+    <h1>炙燒鮭魚三味 3 貫 $180</h1>
+    <img src="../assets/imgs/炙燒比目魚鰭邊肉.jpeg" alt="" width="450" height="">
+    <h1>炙燒鮭魚三味 3 貫 $180</h1>
+    <img src="../assets/imgs/菜單.png" alt="" width="450" height="">
   </div>
 </template>
 
@@ -8,8 +13,8 @@
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+    /* display: flex; */
+    /* align-items: center; */
   }
 }
 </style>
