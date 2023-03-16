ade827cb88c86ee758b0100cfd2dd4edda42087d
egg <egg810207@gmail.com>
2023-03-16 19:05:32 +0800

    新增地圖、基本資料

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,34 +8,24 @@ defineProps({
 </script>
 
 <template lang="pug">
-.greetings
-  h1.green {{ msg }}
-  h3 You&rsquo;ve successfully created a project with
-    a(target='_blank', href='https://vitejs.dev/') Vite +
-    a(target='_blank', href='https://v2.vuejs.org/') Vue 2 .
+div(class='w-full h-full bg-blue-50')
+  div(class='m-5 flex justify-center items-center bg-blue-50')
+    img(alt='logo' src='@/assets/wei120322.jpg' class='w-40 border-double border-2 border-black rounded-full')
+    div(class='ml-5 text-base')
+      p(class='mb-1 text-xl') {{ msg }}
+        // TODO: icon
+        //- i(class="icofont-facebook")
+        a(target='_blank' href='https://www.facebook.com/wei120322' class='ml-1 p-2 text-blue-600 hover:bg-amber-200') FB粉絲團
+      p(class='mb-1') 聯絡電話： 0917-819-904
+      p(class='mb-1') 營業時間： 11:30 ~ 14:00 / 17:30 ~ 21:30
+      p(class='mb-1') 地址： 新北市中和區光華街45號
+  iframe(src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.890818763918!2d121.50338085088123!3d25.003825645575205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a9b12f9101cd%3A0x815bd5e96df3608d!2z5aO95Y-45ayk!5e0!3m2!1szh-TW!2stw!4v1678962000821!5m2!1szh-TW!2stw"
+  class='w-full h-[500px] mt-2'
+  )
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
 @media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    display: block;
-    text-align: left;
-  }
+
 }
 </style>
